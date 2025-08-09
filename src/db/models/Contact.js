652f927483536a02165f4d3930fcec26777b45ac
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-08-09 16:43:53 +0300

    feat: add contact schema and model

---
+++ src/db/models/Contact.js
@@ -0,0 +1,29 @@
+import { model, Schema } from 'mongoose';
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const ContactCollection = model('contact', contactSchema);
+export default ContactCollection;
