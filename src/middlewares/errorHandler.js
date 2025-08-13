5224b43fc32700639a8b67b419f745c71baa5ee6
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-08-13 15:34:02 +0300

    feat: update errorHandler (add err.mesage for property data)

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -5,7 +5,7 @@ export default function (err, req, res, next) {
     res.status(err.status).json({
       status: err.status,
       message: err.name,
-      data: err,
+      data: err.message,
     });
     return;
   }
